213a3b53d1a2cd1fcfc37c1f3308af4dc739ff75
Maciej Kutynia <kutynia.maciej@gmail.com>
2024-09-13 19:40:26 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import process from 'process';
 
 async function bootstrap() {
-  //TODO: dodać cors'y tylko jeden adres przepuścić
+  const isProd = process.env.NODE_ENV === 'production';
+  const corsOptions = {
+    origin: isProd
+      ? ['https://musicapply.mkwsieci.pl']
+      : ['http://localhost:3000'],
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Origin, Authorization, Cookie',
+    preflightContinue: false,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   app.enableCors({ origin: '*' });
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
